75dbb6bfca60667f2d08645aabb3367e0cfbbf42
Andrew Hughes <andrew.hughes@kroger.com>
2022-06-02 10:25:18 -0700

    Load Animals

--- app/src/main/java/com/example/randomiser/ui/HacktivityScreen.kt
+++ app/src/main/java/com/example/randomiser/ui/HacktivityScreen.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
 import coil.compose.AsyncImage
 import com.example.randomiser.HacktivityViewModel
 import com.example.randomiser.model.api.CatData
@@ -18,27 +20,31 @@ fun Layout(viewModel: HacktivityViewModel) {
     Row {
         Text("Title")
     }
-    Animals(viewModel.cat.value, viewModel.dog.value)
+
+    Animals(viewModel.cat, viewModel.dog)
+
     Row {
         Text("Footer")
     }
 }
 
 @Composable
-fun Animals(cat: CatData?, dog: DogData?) {
+fun Animals(cat: LiveData<CatData>, dog: LiveData<DogData>) {
+    val catState = cat.observeAsState().value
+    val dogState = dog.observeAsState().value
     Row {
-        if (cat != null && dog != null) {
+        if (catState != null && dogState != null) {
             Column {
                 AsyncImage(
-                    model = cat.url,
-                    contentDescription = cat.url,
+                    model = catState.url,
+                    contentDescription = catState.url,
                     modifier = Modifier.width(200.dp).height(200.dp)
                 )
             }
             Column {
                 AsyncImage(
-                    model = dog.url,
-                    contentDescription = dog.url,
+                    model = dogState.url,
+                    contentDescription = dogState.url,
                     modifier = Modifier.width(200.dp).height(200.dp)
                 )
             }
